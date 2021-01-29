229757cdee422b190cb43db2fa266cbd60a27df0
Chris H <redpandaxlbusiness@gmail.com>
2021-01-29 00:50:17 -0800

    refactor, creating slice of any size to then append numbers to the index of slice.

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -29,16 +29,39 @@ func TestSum(t *testing.T) {
 		}
 	})
 
-	t.Run("sumAll test", func(t *testing.T) {
-		num1 := []int{1, 2}
-		num2 := []int{0, 9}
+	//t.Run("sumAll test", func(t *testing.T) {
+	//	num1 := []int{1, 2}
+	//	num2 := []int{0, 9}
+	//
+	//	got := SumAll(num1, num2)
+	//	want := []int{3, 9}
+	//
+	//	if !reflect.DeepEqual(got, want) {
+	//		t.Errorf("got %v want %v, given, %v and %v", got, want, num1, num2)
+	//	}
+	//})
 
-		got := SumAll(num1, num2)
-		want := []int{3, 9}
+}
 
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
 		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v, given, %v and %v", got, want, num1, num2)
+			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
 	})
 
 }
